506089ce6113b409401c4779e639fcdd88df2fb8
SamWen0201 <jqwertyu899800@gmail.com>
2026-03-08 20:30:04 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import WhiteBoard from "./components/WhiteBoard.vue";
+
+export default {
+  components: {
+    WhiteBoard,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<template>
+  <main>
+    <h1>Sticky Flash</h1>
+    <WhiteBoard></WhiteBoard>
+  </main>
+</template>
+
+<style scoped></style>
